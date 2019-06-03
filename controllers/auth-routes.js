119aa8b2bb4a060ea1c9e86989dfb633fe1988d8
Alan Perez <alanp9six@gmail.com>
2019-06-03 12:14:53 -0500

    google auth working..

--- controllers/auth-routes.js
+++ controllers/auth-routes.js
@@ -1,5 +1,7 @@
 // creates an instance of the express router
 const router = require('express').Router();
+// passport
+const passport = require('passport');
 
 // auth login
 router.get('/login', (req, res) => {
@@ -13,9 +15,16 @@ router.get('/logout', (req, res) => {
 });
 
 // auth with google+
-router.get('/google', (req, res) => {
-    // handle with passport
-    res.send('logging in with Google');
-});
+router.get('/google', passport.authenticate('google', {
+    scope:["profile"]
+}));
 
+// callback route for google to redirect to
+router.get('/google/redirect',(req,res) => {
+    res.send('You reached the callback URL');
+})
+//auth with facebook
+router.get('/facebook', passport.authenticate('facebook', {
+    scope:['read_stream']
+}));
 module.exports = router;
